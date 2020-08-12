17d7d213bf3eda05049c42ccdbda91685a4f2db9
LiShushi <43929922+LiCarry@users.noreply.github.com>
2020-08-12 17:19:42 +0800

    Change packages names (#4)
    * Upload controller `CommentController`
    
    * Upload three models `Comment` `File` `Group`
    
    * Upload three repositories `Comment` `File` `Group`

---
+++ src/main/java/com/sprint/summerproject/repositories/FileRepository.java
@@ -0,0 +1,9 @@
+package com.sprint.summerproject.repositories;
+
+import com.sprint.summerproject.models.File;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends MongoRepository<File, String> {
+}
---
+++ src/main/java/com/sprint/summerproject/repositories/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.sprint.summerproject.repositories;
+
+import com.sprint.summerproject.models.Group;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends MongoRepository<Group, String> {
+}
