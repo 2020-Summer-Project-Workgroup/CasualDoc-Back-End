2dfa2d53d1c5a0ccf53068fc49892dbdf734b85a
LSS <15510269739@163.com>
2020-08-12 14:43:40 +0800

    Upload controller `CommentController`

---
+++ src/main/java/com/sprint/summerproject/controllers/CommentController.java
@@ -0,0 +1,24 @@
+package com.sprint.summerproject.controllers;
+
+import com.sprint.summerproject.models.Comment;
+import com.sprint.summerproject.repositories.CommentRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+    private final CommentRepository commentRepository;
+
+    public CommentController(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @GetMapping("/all")
+    public List<Comment> getAll() {
+        return commentRepository.findAll();
+    }
+}
