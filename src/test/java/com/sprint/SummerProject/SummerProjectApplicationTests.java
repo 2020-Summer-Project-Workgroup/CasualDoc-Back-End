3d423fec8181c218c43f850bff34dc1c07ca2057
h56983577 <h56983577@126.com>
2020-08-12 11:51:45 +0800

    Test git ignore file.

---
+++ src/test/java/com/sprint/SummerProject/SummerProjectApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sprint.SummerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
---
+++ src/test/java/com/sprint/SummerProject/Utils/CodeGeneratorTest.java
@@ -0,0 +1,13 @@
+package com.sprint.SummerProject.Utils;
+
+
+import com.sprint.SummerProject.utils.CodeGenerator;
+import org.junit.jupiter.api.Test;
+
+public class CodeGeneratorTest {
+
+    @Test
+    public void test() {
+        System.out.println(CodeGenerator.generateCode());
+    }
+}
