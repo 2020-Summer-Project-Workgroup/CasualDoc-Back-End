9150472220574944aca2f168aa6901e505bd8245
mindswapper2049 <jerryloi@163.com>
2020-08-14 05:42:46 +0800

    Add SessionController for Login

---
+++ src/main/java/com/sprint/summerproject/controllers/SessionController.java
@@ -0,0 +1,40 @@
+package com.sprint.summerproject.controllers;
+
+import com.sprint.summerproject.models.User;
+import com.sprint.summerproject.services.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SessionController {
+
+    private final UserService userService;
+
+    public SessionController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/session/tel")
+    public String verifyTel(@RequestParam String tel, @RequestParam String password) {
+        User user = userService.retrieveUserByTel(tel);
+        if (user != null) {
+            return password.equals(user.getPassword()) ? "Yes" : "No";
+        }
+        else {
+            return "No";
+        }
+    }
+
+    @GetMapping("/session/email")
+    public String verifyEmail(@RequestParam String email, @RequestParam String password) {
+        User user = userService.retrieveUserByEmail(email);
+        if (user != null) {
+            return password.equals(user.getPassword()) ? "Yes" : "No";
+        }
+        else {
+            return "No";
+        }
+    }
+
+}
