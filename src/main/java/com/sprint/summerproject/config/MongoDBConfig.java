bb0bee869b197f8c792c98b1a72e545165a8a8da
mindswapper2049 <jerryloi@163.com>
2020-08-14 05:44:43 +0800

    Auto database insertion for testing updated

--- src/main/java/com/sprint/summerproject/config/MongoDBConfig.java
+++ src/main/java/com/sprint/summerproject/config/MongoDBConfig.java
@@ -1,30 +1,21 @@
 package com.sprint.summerproject.config;
 
-import com.sprint.summerproject.models.Comment;
-import com.sprint.summerproject.repositories.CommentRepository;
+import com.sprint.summerproject.models.User;
 import com.sprint.summerproject.repositories.UserRepository;
-import org.bson.types.Binary;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-import java.util.Date;
 
 @EnableMongoRepositories(basePackageClasses = UserRepository.class)
 @Configuration
 public class MongoDBConfig {
 
     @Bean
-    CommandLineRunner commandLineRunner(CommentRepository commentRepository) {
-        byte[] b = new byte[4];
-        b[0] = (byte) (16 & 0xff);
-        b[1] = (byte) (16 >> 8 & 0xff);
-        b[2] = (byte) (16 >> 16 & 0xff);
-        b[3] = (byte) (16 >> 24 & 0xff);
+    CommandLineRunner commandLineRunner(UserRepository userRepository) {
 
         return strings -> {
-            commentRepository.save(new Comment("LSS", new Binary(b), new Binary(b), new Date()));
+            userRepository.save(new User("Jerry", "jerryloi@163.com", "13880265015", "2049", null));
         };
     }
 }
