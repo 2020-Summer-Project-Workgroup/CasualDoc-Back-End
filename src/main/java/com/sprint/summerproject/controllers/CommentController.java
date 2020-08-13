58e65a9eda7fc8a48ab3cf191c714551c7c0af3a
Jerry Loi <45759373+mindswapper2049@users.noreply.github.com>
2020-08-14 05:52:03 +0800

    Login verification support v1.1 (#10)
    * Add SessionController for Login
    
    * Auto database insertion for testing updated
    
    * Normalize routing in CommentController btw

--- src/main/java/com/sprint/summerproject/controllers/CommentController.java
+++ src/main/java/com/sprint/summerproject/controllers/CommentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/comments")
+@RequestMapping("/comment")
 public class CommentController {
     private final CommentRepository commentRepository;
 
