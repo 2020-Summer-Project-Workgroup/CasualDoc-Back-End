1c16c72e92d34661f8e99973516bb863008a6f5d
h56983577 <49408466+h56983577@users.noreply.github.com>
2020-08-14 07:53:45 +0800

    CRUD Code 1.2 (#11)
    * Add Code Storage Container.
    
    * Modify the CodeController, to make it more available user concurrent circumstances.

--- src/main/java/com/sprint/summerproject/services/TelService.java
+++ src/main/java/com/sprint/summerproject/services/TelService.java
@@ -7,23 +7,27 @@ import com.aliyuncs.IAcsClient;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
-import com.sprint.summerproject.models.Code;
+import com.sprint.summerproject.storages.CodeStorage;
 import com.sprint.summerproject.utils.CodeGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+@Service
+public class TelService {
 
-public class CodeService {
+    private final CodeStorage codeStorage;
 
-    private static Map<String, Code> codeMap = new ConcurrentHashMap<String, Code>();
+    @Autowired
+    public TelService(CodeStorage codeStorage) {
+        this.codeStorage = codeStorage;
+    }
 
-    public static void sendCode(String tel) throws ClientException {
+    public void sendCode(String tel) throws ClientException {
         DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "<accessKeyId>", "<accessSecret>");
         IAcsClient client = new DefaultAcsClient(profile);
 
         CommonRequest request = new CommonRequest();
         String code = CodeGenerator.generateCode();
-        Code codeItem = new Code(code);
         request.setSysMethod(MethodType.POST);
         request.setSysDomain("dysmsapi.aliyuncs.com");
         request.setSysVersion("2017-05-25");
@@ -36,16 +40,11 @@ public class CodeService {
 
         CommonResponse response = client.getCommonResponse(request);
         System.out.println(response.getData());
-        codeMap.put(tel, codeItem);
+        codeStorage.put(tel, code);
     }
 
-    public static boolean checkCode(String tel, String code) {
-        Code codeItem;
-        if ((codeItem = codeMap.get(tel)) != null) {
-            return code.equals(codeItem.getCode());
-        } else {
-            return false;
-        }
+    public String retrieveCode(String tel) {
+        return codeStorage.get(tel);
     }
 
 }
