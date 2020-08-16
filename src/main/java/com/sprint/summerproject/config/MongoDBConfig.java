27b5e7912af57aadffe5d0e1a00de4de1ab65206
mindswapper2049 <jerryloi@163.com>
2020-08-16 21:41:51 +0800

    This is for testing

--- src/main/java/com/sprint/summerproject/config/MongoDBConfig.java
+++ src/main/java/com/sprint/summerproject/config/MongoDBConfig.java
@@ -1,5 +1,6 @@
 package com.sprint.summerproject.config;
 
+import com.sprint.summerproject.models.File;
 import com.sprint.summerproject.models.User;
 import com.sprint.summerproject.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -7,16 +8,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @EnableMongoRepositories(basePackageClasses = UserRepository.class)
 @Configuration
 public class MongoDBConfig {
 
     @Bean
     CommandLineRunner commandLineRunner(UserRepository userRepository) {
+        List<File> files = new ArrayList<>();
 
         return strings -> {
-            userRepository.save(new User("Jerry", "jerryloi@163.com", "13880265015", "2049", null));
-            userRepository.save(new User("Richard", "richard@163.com", "50062998023", "1167", null));
+            userRepository.save(new User("Kawasaki", "kawasaki@gmail.com", "12887364020", "2049", files));
+            userRepository.save(new User("Richard", "richard@163.com", "50062998023", "1167", files));
         };
     }
 }
