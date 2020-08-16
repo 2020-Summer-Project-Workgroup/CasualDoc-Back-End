f8820b47b43f04742aa342700e9f6ddacc1439be
h56983577 <49408466+h56983577@users.noreply.github.com>
2020-08-16 20:57:20 +0800

    Add CORS configure class. (#13)

---
+++ src/main/java/com/sprint/summerproject/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.sprint.summerproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    static final String ORIGINS[] = new String[] { "GET", "POST", "PUT", "DELETE" };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowCredentials(true)
+                .allowedMethods(ORIGINS)
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
