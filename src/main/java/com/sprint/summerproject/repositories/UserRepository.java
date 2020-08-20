ec9081c7113ace0a7e68d08d8d48e2bda413ae0b
h56983577 <h56983577@126.com>
2020-08-20 08:37:32 +0800

    add regex match.

--- src/main/java/com/sprint/summerproject/repositories/UserRepository.java
+++ src/main/java/com/sprint/summerproject/repositories/UserRepository.java
@@ -4,9 +4,12 @@ import com.sprint.summerproject.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     public User findUserById(String id);
     public User findUserByTel(String tel);
     public User findUserByEmail(String email);
+    List<User> findUsersByTelRegex(String regex);
 }
